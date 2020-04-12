88311361a3beab5535a9e3b21e82055fe783ae5b
Danny <dantes817@gmail.com>
2020-04-12 17:52:32 +0200

    was added a few vue elements

--- client/frontend.js
+++ client/frontend.js
@@ -1,3 +1,38 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
-console.log(Vue);
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: [
+                {id: 1, name: 'Danny', value: '+00-234-345-23', marked: false}
+            ]
+        }
+    },
+    computed: {
+        canCreate() {
+            return this.form.value.trim() && this.form.name.trim();
+        }
+
+    },
+    methods: {
+        createContact() {
+            const {...contact} = this.form;
+            console.log(contact);
+
+            this.contacts.push({...contact, id: Date.now(), marked: false});
+            this.form.name = this.form.value = '';
+        },
+        markContact(id) {
+            const contact = this.contacts.find(c => c.id === id);
+            contact.marked = true;
+        },
+        removeContact(id) {
+            this.contacts = this.contacts.filter(c => c.id !== id);
+        }
+    }
+})
